8a3c78362755d5a6d925bad656b31e8aa95e8a2b
Akash Biswas <akashrahul2006@gmail.com>
2025-03-26 00:39:18 +0530

    new change

--- Frontend/src/Components/Layout.jsx
+++ Frontend/src/Components/Layout.jsx
@@ -5,14 +5,13 @@ import Navbar from "./Navbar";
 
 const Layout = () => {
   return (
-    <div className="h-screen w-screen cc">
+    <div className="h-full w-screen cc">
       <div className="mobile_Screen md:border md:rounded-md cc overflow-hidden">
         <div className="h-full w-full flex flex-col justify-between">
           <Header icon={"logo"} text={"Qubiko AI"} />
-          {/* <div className="h-full overflow-auto">
+          <div className="h-full overflow-auto">
             <Outlet />
-          </div> */}
-           <Navbar />
+          </div>
           <Navbar />
         </div>
       </div>

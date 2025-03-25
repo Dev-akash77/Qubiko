2622375bfe18d1c7bd177d85ec2d90949e9ce360
Akash Biswas <akashrahul2006@gmail.com>
2025-03-26 00:36:03 +0530

    test

--- Frontend/src/Components/Layout.jsx
+++ Frontend/src/Components/Layout.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import Navbar from './Navbar';
-import Header from './Header';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const Layout = () => {
   return (
     <div className="h-screen w-screen cc">
-    <div className="mobile_Screen md:border md:rounded-md cc overflow-hidden">
-      <div className="h-full w-full flex flex-col justify-between">
-        <Header icon={"logo"} text={"Qubiko AI"}/>
-         <div className="h-full overflow-auto"> <Outlet />  <Outlet /></div>
-        <Navbar />
+      <div className="mobile_Screen md:border md:rounded-md cc overflow-hidden">
+        <div className="h-full w-full flex flex-col justify-between">
+          <Header icon={"logo"} text={"Qubiko AI"} />
+          <div className="h-full overflow-auto">
+            <Outlet />
+          </div>
+        
+          <h1> nav bar</h1>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Layout
-
-
-
+export default Layout;

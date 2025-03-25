fafbed80940284c9c71c863b4afc26531fec37ac
Akash Biswas <akashrahul2006@gmail.com>
2025-03-26 00:12:48 +0530

    mobile view fixed

--- Frontend/src/Components/Layout.jsx
+++ Frontend/src/Components/Layout.jsx
@@ -6,10 +6,10 @@ import { Outlet } from 'react-router-dom';
 const Layout = () => {
   return (
     <div className="h-screen w-screen cc">
-    <div className="mobile_Screen md:border md:rounded-md cc">
+    <div className="mobile_Screen md:border md:rounded-md cc overflow-hidden">
       <div className="h-full w-full flex flex-col justify-between">
         <Header icon={"logo"} text={"Qubiko AI"}/>
-        <Outlet />
+         <div className="h-full overflow-auto"> <Outlet />  <Outlet /></div>
         <Navbar />
       </div>
     </div>
